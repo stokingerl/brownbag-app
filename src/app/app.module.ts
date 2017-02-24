d21ab111435274955d66b7a498e901e7a37bb878
stokingerl <liamstokinger@gmail.com>
2017-02-23 23:49:07 -0500

    initial app with basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { BlueComponent } from './blue';
+import { RedComponent } from './red';
+
+const appRoutes: Routes = [
+  { path: 'blue', component: BlueComponent },
+  { path: 'red', component: RedComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlueComponent,
+    RedComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
